76e84df47a14f279cf4522ec3ffc1ae4a86c73e2
Mitha Katrapati <91805215+mithak16@users.noreply.github.com>
2025-11-16 11:20:06 -0500

    Update main.jsx
    group chat feature added.

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.jsx";
+import GroupChat from "./pages/GroupChat.jsx";
 import "./index.css";
 
 import { HeroUIProvider } from "@heroui/react";
@@ -8,7 +11,15 @@ import { HeroUIProvider } from "@heroui/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HeroUIProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          {/* Main app with tabs */}
+          <Route path="/" element={<App />} />
+
+          {/* Dynamic group chat route */}
+          <Route path="/chat/:groupName" element={<GroupChat />} />
+        </Routes>
+      </BrowserRouter>
     </HeroUIProvider>
   </React.StrictMode>
 );
